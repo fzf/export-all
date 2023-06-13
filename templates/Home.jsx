af265996cc0238d0748ec650d6cbdc87ddf12cb9
Fletcher Fowler <fletch@fzf.me>
2023-06-13 13:42:12 -0400

    Initial Export All app

---
+++ templates/Home.jsx
@@ -0,0 +1,11 @@
+Template = (data, context) => {
+
+    const K = Klutch
+
+    return (
+        <K.KView style={{ flex: 1}}>
+            <K.KBigTextInput>Coolest Panel Ever</K.KBigTextInput>
+        </K.KView>
+    )
+}
+
